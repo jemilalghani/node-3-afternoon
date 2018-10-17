884c2504f4b479569fa687a1769250e16ee3c14a
Jemila Al-Ghani <jalghani@Jemilas-MacBook-Pro.local>
2018-10-16 19:39:35 -0700

    Add to Backend

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const checkForSession = require('./middleware/checkForSession');
+const swagController = require('./controllers/swag_controller');
+const authController = require('./controllers/auth_controller');
+const cartController = require('./controllers/cart_controller');
+const searchController = require('./controllers/search_controller');
+
+require('dotenv').config();
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000*60*60*24*14
+    }
+}));
+app.use(checkForSession);
+app.use( express.static( `${__dirname}/build` ) );
+
+app.get('/api/swag', swagController.read);
+app.post('/api/login', authController.login);
+app.post('/api/register', authController.register);
+app.post('/api/signout', authController.signout);
+app.get('/api/user', authController.getUser);
+app.post('/api/cart', cartController.add);
+app.post('/api/cart/checkout', cartController.checkout);
+app.delete('/api/cart', cartController.delete);
+app.get('/api/search', searchController.search);
+
+
+
+
+const port = process.env.SERVER_PORT || 3000
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+})
